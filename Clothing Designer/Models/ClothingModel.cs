1822218891b3ae70e4678444a0069814e2ed80c5
Juan Manuel Davila Jr <Juan.M.Davila.Jr@hotmail.com>
2022-04-25 16:32:23 -0500

    Add project files.

---
+++ Clothing Designer/Models/ClothingModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Clothing_Designer.Models
+{
+    public class ClothingModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+
+        public string Description { get; set; }
+
+        public string Style { get; set; }
+
+        public string Size { get; set; }
+
+    }
+}
